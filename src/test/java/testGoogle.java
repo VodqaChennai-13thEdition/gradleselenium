59280994f172987710a8074be619c8970fcedc77
vijayaragavan <vijayaragavan.g@gmail.com>
2017-04-19 17:34:25 +0530

    updated the selenium version

--- src/test/java/testGoogle.java
+++ src/test/java/testGoogle.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created by Vijayaragavan on 18/04/17.
@@ -10,12 +12,13 @@ public class testGoogle {
     @Test
     public void test() {
 
-//Mac os
+         //Firefox 47 and above
 //        System.setProperty("webdriver.gecko.driver", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
-// Windows
-//        System.setProperty("webdriver.gecko.driver","path of geckodriver.exe");
+        System.setProperty("webdriver.firefox.marionette", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
+        DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        WebDriver driver = new FirefoxDriver(capabilities);
 
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.google.com");
 
         driver.close();
