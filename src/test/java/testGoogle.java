332e5520a372c929133812057b63feb0c4a1f1be
vijayaragavan <vijayaragavan.g@gmail.com>
2017-04-21 11:51:03 +0530

    first commit

--- src/test/java/testGoogle.java
+++ src/test/java/testGoogle.java
@@ -1,4 +1,6 @@
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.firefox.MarionetteDriver;
@@ -12,17 +14,33 @@ public class testGoogle {
     @Test
     public void test() {
 
+        long startTime = 0;
+        long stopTime = 0;
+
+
+
+        StopWatch time = new StopWatch();
          //Firefox 47 and above
-        System.setProperty("webdriver.gecko.driver", "/Users/antosukesh/Downloads/geckodriver");
-        //System.setProperty("webdriver.firefox.marionette", "/Users/antosukesh/Downloads/geckodriver");
+//        System.setProperty("webdriver.gecko.driver", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
+        System.setProperty("webdriver.firefox.marionette", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
         DesiredCapabilities capabilities=DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", true);
         WebDriver driver = new FirefoxDriver(capabilities);
 
+
+        startTime = System.currentTimeMillis();
+
         driver.get("http://www.google.com");
 
-        //driver.close();
-        //driver.quit();
+        stopTime = System.currentTimeMillis();
+
+
+        System.out.println("*****Time taken"+ (stopTime-startTime));
+
+
+
+        driver.close();
+        driver.quit();
     }
 
 }
