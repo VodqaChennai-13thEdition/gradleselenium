e77dc47be8562d466323f99f4290a4819e025810
vijayaragavan <vijayaragavan.g@gmail.com>
2017-04-21 12:06:32 +0530

    cleaning up the scripts

--- src/test/java/testGoogle.java
+++ src/test/java/testGoogle.java
@@ -14,12 +14,6 @@ public class testGoogle {
     @Test
     public void test() {
 
-        long startTime = 0;
-        long stopTime = 0;
-
-
-
-        StopWatch time = new StopWatch();
          //Firefox 47 and above
 //        System.setProperty("webdriver.gecko.driver", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
         System.setProperty("webdriver.firefox.marionette", "/Users/Vijayaragavan/Downloads/Vodqa-Softwares/geckodriver");
@@ -28,16 +22,9 @@ public class testGoogle {
         WebDriver driver = new FirefoxDriver(capabilities);
 
 
-        startTime = System.currentTimeMillis();
-
         driver.get("http://www.google.com");
 
-        stopTime = System.currentTimeMillis();
-
-
-        System.out.println("*****Time taken"+ (stopTime-startTime));
-
-
+       
 
         driver.close();
         driver.quit();
